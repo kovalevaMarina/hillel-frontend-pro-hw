a831ec06adf6f2183a3602e79589e3d84a029fb1
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-04-19 14:10:25 +0300

    Add var to for

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
 // ДЗ 3. Странный калькулятор
-var operation,
-    count,
-    result;
+var operation, count, result;
 
-for(;;) {
-  operation = prompt("Выбирите любую из операций +, -, * или /")
-  if (operation === "+" || operation === "-" || operation === "/" || operation === "*") break;
+for (;;) {
+  operation = prompt("Выбирите любую из операций +, -, * или /");
+  if (
+    operation === "+" ||
+    operation === "-" ||
+    operation === "/" ||
+    operation === "*"
+  )
+    break;
 }
 
-for(;;) {
-  count = +prompt("Сколько операндов вы хотите использовать?")
+for (;;) {
+  count = +prompt("Сколько операндов вы хотите использовать?");
   if (count > 1) break;
 }
 
-for(i = 0; i < count; i++) {
-  var operand = +prompt("Введите каждый из операндов")
+for (var i = 0; i < count; i++) {
+  var operand = +prompt("Введите каждый из операндов");
   if (i === 0) {
-    result = operand; continue
+    result = operand;
+    continue;
   }
-  switch(operation) {
-    case "+": result += operand
+  switch (operation) {
+    case "+":
+      result += operand;
       break;
-    case "-": result -= operand 
+    case "-":
+      result -= operand;
       break;
-    case "*": result *= operand
+    case "*":
+      result *= operand;
       break;
-    case "/": result /= operand
+    case "/":
+      result /= operand;
       break;
   }
 }
