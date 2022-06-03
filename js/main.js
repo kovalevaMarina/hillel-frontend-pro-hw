f8f461977e215104f3a6f6fceebcd8a738ef11c2
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-06-03 19:35:24 +0300

    Started to do hw-16

--- js/main.js
+++ js/main.js
@@ -1,40 +1,55 @@
-// Задача 1. Дан HTML код. Поменяйте содержимое элементов с классом zzz
-//на их порядковый номер в коде.
+var productForm = document.forms.product;
+var nameInput = productForm.name;
+var descriptionMsg = productForm.description;
+var priceInput = productForm.price;
+var measureInput = productForm.measure;
+var linkInput = productForm.link;
 
-function changeElementText() {
-  var elems = document.getElementsByClassName("zzz");
-  for (var i = 0; i < elems.length; i++) {
-    elems[i].innerHTML = i + 1;
-  }
+// проверка на Caps Lock для input/textarea
+
+function capitalazeFirstLetters(str) {
+  return str.replace(/([A-Z]|[А-ЯЁ])[A-ZА-ЯЁ]{1,}/g, function ($1) {
+    var resultStr = $1.charAt(0).toUpperCase() + $1.substr(1).toLowerCase();
+    return resultStr;
+  });
 }
-changeElementText();
 
-// Задача 2. Дан HTML код. Поменяйте цвет текста на синий у элементов с текстом www.
+nameInput.addEventListener("change", function (e) {
+  var regex = /^.{1,25}/;
+  var result = e.target.value.match(regex) || [];
+  e.target.value = capitalazeFirstLetters(result[0]);
+});
 
-function changeColorText() {
-  var children = document.body.children;
-  for (node of children) {
-    if (node.textContent.includes("www")) {
-      node.style.color = "blue";
-    }
+descriptionMsg.addEventListener("change", function (e) {
+  var regexDesc = /^.{1,150}/;
+  var resultDesc = e.target.value.match(regexDesc);
+  e.target.value = capitalazeFirstLetters(resultDesc[0]);
+});
+
+measureInput.addEventListener("change", function (e) {
+  var regexMeasure = /^(шт|кг|л|ед)$/;
+  var resultMeasure = e.target.value.match(regexMeasure) || [];
+  if (resultMeasure[0]) {
+    e.target.value = resultMeasure[0];
+    measureInput.style.borderColor = "black";
+  } else {
+    measureInput.style.borderColor = "red";
   }
+});
+
+priceInput.addEventListener("change", function (e) {
+  var regexPrice = /^\d*(\,|\.)\d{2}/;
+  var resultPrice = e.target.value.match(regexPrice) || [];
+  e.target.value = resultPrice
+    ? resultPrice[0]
+    : Math.floor(e.target.value * 100) / 100;
+  console.log(resultPrice);
+});
+
+// e.target.value + ".00"
+
+function isValidURL(string) {
+  var res = string.test(/http\:\/\//g);
+  return res !== null;
 }
-changeColorText();
-
-// Задача 3. Дан HTML код. Сгенерируйте новый список (ul > li) используя содержимое элементов с классом zzz.
-
-function createList() {
-  var items = document.querySelectorAll(".zzz");
-  var ul = document.createElement("ul");
-  var body = document.body;
-  body.prepend(ul);
-
-  function createListItem(item) {
-    var li = document.createElement("li");
-    ul.append(li);
-    var textValue = item.textContent;
-    li.textContent = textValue;
-  }
-  items.forEach(createListItem);
-}
-createList();
+console.log(isValidURL("http://www.nine.com.ua/uk"));
