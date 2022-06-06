5838debcd3a6ac5ab8c5b333217c7c0d49aba0c7
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-06-06 18:50:04 +0300

    Finished hw-16

--- js/main.js
+++ js/main.js
@@ -4,52 +4,65 @@ var descriptionMsg = productForm.description;
 var priceInput = productForm.price;
 var measureInput = productForm.measure;
 var linkInput = productForm.link;
+var submit = productForm.submit;
 
 // проверка на Caps Lock для input/textarea
-
 function capitalazeFirstLetters(str) {
   return str.replace(/([A-Z]|[А-ЯЁ])[A-ZА-ЯЁ]{1,}/g, function ($1) {
-    var resultStr = $1.charAt(0).toUpperCase() + $1.substr(1).toLowerCase();
-    return resultStr;
+    return $1.charAt(0).toUpperCase() + $1.substr(1).toLowerCase();
   });
 }
 
-nameInput.addEventListener("change", function (e) {
-  var regex = /^.{1,25}/;
-  var result = e.target.value.match(regex) || [];
-  e.target.value = capitalazeFirstLetters(result[0]);
-});
+productForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-descriptionMsg.addEventListener("change", function (e) {
-  var regexDesc = /^.{1,150}/;
-  var resultDesc = e.target.value.match(regexDesc);
-  e.target.value = capitalazeFirstLetters(resultDesc[0]);
-});
-
-measureInput.addEventListener("change", function (e) {
-  var regexMeasure = /^(шт|кг|л|ед)$/;
-  var resultMeasure = e.target.value.match(regexMeasure) || [];
-  if (resultMeasure[0]) {
-    e.target.value = resultMeasure[0];
-    measureInput.style.borderColor = "black";
+  // название товара
+  var regexName = /^.{1,25}/;
+  var resultName = nameInput.value.match(regexName) || [];
+  if (resultName.length == 0) {
+    nameInput.style.borderColor = "red";
   } else {
-    measureInput.style.borderColor = "red";
+    nameInput.value = capitalazeFirstLetters(resultName[0]);
+    nameInput.style.borderColor = "black";
   }
+
+  // цена товара
+  var regexPrice = /(^\d{0,}(\,|\.)\d{2})|\d{1,}/;
+  var resultPrice = priceInput.value.match(regexPrice) || [];
+  if (resultPrice.length == 0) {
+    priceInput.style.borderColor = "red";
+  } else {
+    priceInput.style.borderColor = "black";
+    priceInput.value = resultPrice
+      ? resultPrice[0]
+      : Math.floor(priceInput * 100) / 100;
+  }
+
+  // единица измерения товара
+  var regexMeasure = /^(шт|кг|л|ед)$/;
+  var resultMeasure = measureInput.value.match(regexMeasure) || [];
+  console.log(resultMeasure);
+  if (resultMeasure.length == 0) {
+    measureInput.style.borderColor = "red";
+  } else {
+    measureInput.value = resultMeasure[0];
+    measureInput.style.borderColor = "black";
+  }
+
+  // описание товара
+  var regexDesc = /^.{1,150}/;
+  var resultDesc = descriptionMsg.value.match(regexDesc) || [];
+  if (resultDesc.length == 0) {
+    descriptionMsg.style.borderColor = "red";
+  } else {
+    descriptionMsg.value = capitalazeFirstLetters(resultDesc[0]);
+    descriptionMsg.style.borderColor = "black";
+  }
+
+  // ссылка на товар
+  var regexURL = /^(http|https)\:\/\//;
+  !regexURL.test(linkInput.value)
+    ? (linkInput.value = "https://" + linkInput.value)
+    : linkInput.value;
 });
-
-priceInput.addEventListener("change", function (e) {
-  var regexPrice = /^\d*(\,|\.)\d{2}/;
-  var resultPrice = e.target.value.match(regexPrice) || [];
-  e.target.value = resultPrice
-    ? resultPrice[0]
-    : Math.floor(e.target.value * 100) / 100;
-  console.log(resultPrice);
-});
-
-// e.target.value + ".00"
-
-// function isValidURL(string) {
-//   var res = string.test(/http\:\/\//g);
-//   return res !== null;
-// }
-// console.log(isValidURL("http://www.nine.com.ua/uk"));
+// название товара
