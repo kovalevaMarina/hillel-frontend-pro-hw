f1b83152cc6ef528096e6367e5bfb96f990dbe08
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-06-15 23:46:55 +0300

    Return hw-18

--- js/MammalSquirrel.js
+++
@@ -1,9 +0,0 @@
-class MammalSquirrel extends Mammal {
-  constructor(specie, favoriteMeals, speed, hasTail, fluffyWool) {
-    super(specie, favoriteMeals, speed, hasTail);
-    this.fluffyWool = fluffyWool;
-  }
-  jump() {
-    return "Squirrel can jump three to four meters forward!";
-  }
-}
--- js/ReptiliaSnake.js
+++
@@ -1,9 +0,0 @@
-class ReptiliaSnake extends Reptilia {
-  constructor(specie, favoriteMeals, amountPaws, isHunter, hasPoisson) {
-    super(specie, favoriteMeals, amountPaws, isHunter);
-    this.hasPoisson = hasPoisson;
-  }
-  bite() {
-    return this.hasPoisson ? "You are died..." : "You are lucky!";
-  }
-}
