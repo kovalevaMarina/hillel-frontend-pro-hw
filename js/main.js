2a79fd1929b77a3e006f618c8a446ad6584c73ab
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-06-15 23:50:04 +0300

    Return do hw-18

--- js/main.js
+++ js/main.js
@@ -1,13 +1,64 @@
-class Reptilia extends Animal {
-  constructor(specie, favoriteMeals, amountPaws, isHunter) {
-    super(specie, favoriteMeals);
-    this.amountPaws = amountPaws;
-    this.isHunter = isHunter;
-  }
-  plantEggs() {
-    const min = 1;
-    const max = 5;
-    let quantityEggs = Math.floor(Math.random() * (max - min + 1)) + min;
-    return `The ${this.specie} lay ${quantityEggs} eggs in year.`;
-  }
+/* 
+Задача.
+Реализуйте сущность Worker (Работник), который будет иметь следующие свойства: 
+name (имя), surname (фамилия), rate (ставка за день работы), days (количество отработанных 
+дней). Также сущность должна иметь метод getSalary(), который будет выводить зарплату работника.
+Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
+Так же добавить несколько ролей. Администратор, Кассир, Консультант.
+И добавить для каждого уникальные свойства.
+Учесть все 4 принципа ООП.
+*/
+const worker = {
+  name: "",
+  surname: "",
+  rate: "",
+  days: "",
+  role: "",
+  getSalary: () => {
+    return this.rate * this.days;
+  },
+  getListEmployeers: () => {},
+  getListProducts: () => {},
+  countAward: () => {
+    let count = 0;
+    return {
+      add: (amount) => {
+        count += amount;
+      },
+      decrease: (amount) => {
+        count -= amount;
+      },
+      getCount: () => count,
+    }
+  },
+};
+administrator(worker) {
+  addEmployees: () => {},
+  changeListProducts: () => {},
+  getListEmployeers: () => {
+    return [];
+  },
+  getListProducts: () => {
+    return [];
+  },
+}
+
+cashier(worker) {
+  makeSalaryPayment: () => {},
+  getListEmployeers: () => {
+    return [];
+  },
+  getListProducts: () => {
+    return [];
+  },
+}
+
+consultant (worker) {
+  countOfProductsSold: () => {},
+  getListEmployeers: () => {
+    return null
+  },
+  getListProducts: (id) => {
+    return [];
+  },
 }
