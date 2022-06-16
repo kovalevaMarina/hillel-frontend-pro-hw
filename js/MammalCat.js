f8e4646a62c879403b6b70b60bfeeb825152dc36
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-06-16 11:01:45 +0300

    Update class MammalCat

--- js/MammalCat.js
+++ js/MammalCat.js
@@ -1,10 +1,12 @@
 class MammalCat extends Mammal {
-  constructor(specie, favoriteMeals, speed, hasTail, breed, color) {
+  constructor(specie, favoriteMeals, speed, hasTail, isSleep, color) {
     super(specie, favoriteMeals, speed, hasTail);
-    this.breed = breed;
+    this.isSleep = isSleep;
     this.color = color;
   }
   sleep(hours) {
-    return `The ${this.breed} cats can sleep about ${hours} hours every day.`;
+    return this.isSleep
+      ? `Can not touch me! I am sleeping only ${hours} hours.`
+      : `I slept ${hours} hours. It's enough for me. Play with me!`;
   }
 }
